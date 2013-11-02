cc6064b6311b30a209c85289a87c377d648c844d
Marco Fontana <mfontana@cloudbasesolutions.com>
2013-11-02 11:38:42 +0100

    2° step

--- sp_prep.ps1
+++ sp_prep.ps1
@@ -1,14 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
-
-\f0\fs24 \cf0 Add-PSSnapin Microsoft.SharePoint.PowerShell\
+Add-PSSnapin Microsoft.SharePoint.PowerShell\
 $FarmCredential = Get-Credential -credential cloudbasesolutions\\spFarm\
 $Passphrase = Read-Host -assecurestring "Sharepoitn Pass Phrase"\
-$DbName = "SP_Extranet_Config"\
-$DbServer = \'93<sql_server_name>\'94\
+$DbName = "SP_Config"
+$DbServer = <sql_server_name>
 $AdminContentDb = "SP_Extranet_AdminContent"\
 New-SPConfigurationDatabase -DatabaseName $DbName -DatabaseServer $DbServer -AdministrationContentDatabaseName $AdminContentDb -FarmCredentials $FarmCredential -Passphrase $Passphrase\
 Install-SPHelpCollection -All\
